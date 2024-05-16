803f4b34f868db5ffd027e0c404680ef22d4f575
Taifinkaba <taifinkabad@gmail.com>
2024-05-15 20:37:40 -0400

    Q1

--- src/modify.js
+++ src/modify.js
@@ -1,5 +1,15 @@
+// clicker Q1 
+const clickButton = document.getElementById('click-button');
+
 const clickCounterHandler = () => {
+  let click = parseInt(clickButton.getAttribute('data-clicks'));
+  click += 1;
+  clickButton.setAttribute('data-clicks', click.toString());
+  clickButton.textContent = `Clicked: ${click} time${click !== 1 ? 's' : ''}`;
 };
+
+clickButton.addEventListener('click', clickCounterHandler);
+
 
 const handleKeydown = () => {
 };
