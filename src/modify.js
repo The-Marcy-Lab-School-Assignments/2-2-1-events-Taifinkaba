8e6920bf865b8f7ef9df90ae76d3129083a26863
Taifinkaba <taifinkabad@gmail.com>
2024-05-15 21:33:50 -0400

    Q4

--- src/modify.js
+++ src/modify.js
@@ -7,21 +7,26 @@ const clickCounterHandler = () => {
   clickButton.setAttribute('data-clicks', click.toString());
   clickButton.textContent = `Clicked: ${click} time${click !== 1 ? 's' : ''}`;
 };
-
 clickButton.addEventListener('click', clickCounterHandler);
 
+// Q3  cant do
 
+
+// Q2
 const handleKeydown = (event) => {
   const keydownTracker = document.getElementById('keydown-tracker');
   keydownTracker.textContent = `You pressed ${event.code}`;
 };
-
 document.body.addEventListener('keydown', handleKeydown);
 
+// Q4
 const handleDelegation = (event) => {
   const resultSpan = document.querySelector('#delegation-result');
-  resultSpan.textContent = event.target.textContent;
+  if (event.target.tagName === 'BUTTON') {
+    resultSpan.textContent = event.target.textContent;
+  }
 };
+
 
 const addNewRandomNumber = () => {
 };
